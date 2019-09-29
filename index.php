bad06fb77098d890a9516e16bf16513c78bfa435
julio <55961071+julio956@users.noreply.github.com>
2019-09-29 23:47:51 +0200

    Add files via upload
    Starting of project

---
+++ index.php
@@ -0,0 +1,439 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Testrequest</title>
+    <link rel="icon" href="img/favicon.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <!-- themify CSS -->
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="css/flaticon.css">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="css/slick.css">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index_modif.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css">
+    <link rel="stylesheet" href="css/testimony_slide.css">
+
+</head>
+
+<body>
+    <!--::header part start::-->
+    <header class="main_menu home_menu">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="index.html"> <img src="img/favicon.png" alt="logo">Test requeste </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse main-menu-item justify-content-end" id="navbarSupportedContent">
+                            <ul class="navbar-nav align-items-center">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="index.php">Home</a>
+                                </li>
+                               
+                                <li class="d-none d-lg-block">
+                                    <a class="btn_1" href="#">Login</a>
+                                </li>
+
+                                <li class="d-none d-lg-block">
+                                    <a class="btn_1" href="#">S'inscrire</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header part end-->
+
+    <!-- banner part start-->
+    <section class="banner_part">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-xl-6">
+                    <div class="banner_text">
+                        <div class="banner_text_iner">
+
+                            <h1>Test Request</h1>
+                            <p>Nous sommes une plate-forme qui vous accompagne dans vos études en mettant à votre disposition
+                                des épreuves et des documents afin que ne manquiez de rien dans vos parcours académiques.</p>
+                            <a href="#" class="btn_1">S'inscrire </a>
+                            <a href="#epreuve" class="btn_2">Voir les épreuves </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div></div>
+    </section>
+    <!-- banner part start-->
+
+
+    <!-- upcoming_event part start-->
+    <section>
+
+        <div class="container">
+            <div class='row'>
+                <div class="col-lg-12">
+                    <div class="blog_right_sidebar">
+                        <aside class="single_sidebar_widget search_widget">
+                            <form action="#">
+                                <div class='container_fluid'>
+                                    <div class='row'>
+                                        <div class='col-lg-3'><input class='ressearch_input' type="text" onfocus="this.placeholder = 'Entrez la matière'" onblur="this.placeholder = 'Entrez la matière'"></div>
+                                        <div class='col-lg-3'><input type="text" class='ressearch_input' onfocus="this.placeholder = 'Notions recherchées'" onblur="this.placeholder = 'Notions recherchées'"></div>
+                                        <div class='col-lg-1'><input type="number" class='ressearch_input' min=1995 max=></div>
+                                        <div class='col-lg-2'><input type="text" class='ressearch_input' onfocus="this.placeholder = 'Niveaux'" onblur="this.placeholder = 'Niveaux'"></div>
+                                        <div class='col-lg-3'><button class="button rounded-0 primary-bg text-white w-100 btn_1" type="submit"> <span class="glyphicon glyphicon-search"></span> Search</button></div>
+
+                                    </div>
+                                </div>
+                            </form>
+                        </aside>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <!--::review_part start::-->
+    <section class="special_cource padding_top" style='position:relative; bottom:85px;'>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-5">
+                    <div class="section_tittle text-center">
+
+                        <h2>Les épreuves à la une</h2>
+                    </div>
+                </div>
+            </div>
+    </section>
+    <section style='position:relative;bottom:120px;'>
+        <div class='container section_epreuve'>
+            <!--//      Début de la premiere d'affichage des epreuves     //-->
+            <div class='row'>
+                <div class='col-md-3 une_epreuve'>
+                    <div class='epreuve_fond'></div>
+                    <div class="epreuve_info">
+                        <span class="matiere">Matière</span><br>
+                        <span class='classe'>Terminale</span>
+                    </div>
+                </div>
+                <div class='col-md-3 une_epreuve'>
+                    <div class='epreuve_fond'></div>
+                    <div class="epreuve_info">
+                        <span class="matiere">Matière</span><br>
+                        <span class='classe'>Terminale</span>
+                    </div>
+                </div>
+                <div class='col-md-3 une_epreuve'>
+                    <div class='epreuve_fond'></div>
+                    <div class="epreuve_info">
+                        <span class="matiere">Matière</span><br>
+                        <span class='classe'>Terminale</span>
+                    </div>
+                </div>
+                <div class='col-md-3 une_epreuve'>
+                    <div class='epreuve_fond'></div>
+                    <div class="epreuve_info">
+                        <span class="matiere">Matière</span><br>
+                        <span class='classe'>Terminale</span>
+                    </div>
+                </div>
+
+
+            </div>
+            <!--//     Fin de l'affichage          //-->
+            <!--// La seconde ligne//-->
+            <div class='row une_ligne_epreuve'>
+                <div class='col-md-3 une_epreuve'>
+                    <div class='epreuve_fond'></div>
+                    <div class="epreuve_info">
+                        <span class="matiere">Matière</span><br>
+                        <span class='classe'>Terminale</span>
+                    </div>
+                </div>
+                <div class='col-md-3 une_epreuve'>
+                    <div class='epreuve_fond'></div>
+                    <div class="epreuve_info">
+                        <span class="matiere">Matière</span><br>
+                        <span class='classe'>Terminale</span>
+                    </div>
+                </div>
+                <div class='col-md-3 une_epreuve'>
+                    <div class='epreuve_fond'></div>
+                    <div class="epreuve_info">
+                        <span class="matiere">Matière</span><br>
+                        <span class='classe'>Terminale</span>
+                    </div>
+                </div>
+                <div class='col-md-3 une_epreuve'>
+                    <div class='epreuve_fond'></div>
+                    <div class="epreuve_info">
+                        <span class="matiere">Matière</span><br>
+                        <span class='classe'>Terminale</span>
+                    </div>
+                </div>
+
+
+            </div>
+            <!--Fin-->
+            <!--Le troisième-->
+            <div class='row une_ligne_epreuve'>
+                <div class='col-md-3 une_epreuve'>
+                    <div class='epreuve_fond'></div>
+                    <div class="epreuve_info">
+                        <span class="matiere">Matière</span><br>
+                        <span class='classe'>Terminale</span>
+                    </div>
+                </div>
+                <div class='col-md-3 une_epreuve'>
+                    <div class='epreuve_fond'></div>
+                    <div class="epreuve_info">
+                        <span class="matiere">Matière</span><br>
+                        <span class='classe'>Terminale</span>
+                    </div>
+                </div>
+                <div class='col-md-3 une_epreuve'>
+                    <div class='epreuve_fond'></div>
+                    <div class="epreuve_info">
+                        <span class="matiere">Matière</span><br>
+                        <span class='classe'>Terminale</span>
+                    </div>
+                </div>
+                <div class='col-md-3 une_epreuve'>
+                    <div class='epreuve_fond'></div>
+                    <div class="epreuve_info">
+                        <span class="matiere">Matière</span><br>
+                        <span class='classe'>Terminale</span>
+                    </div>
+                </div>
+
+
+            </div>
+            <!--Fin-->
+        </div>
+
+
+    </section>
+
+    <!-- learning part end-->
+
+    <!-- member_counter counter start -->
+    <section class="member_counter">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single_member_counter">
+                        <span class="counter">9024</span>
+                        <h4>Nombre de téléchargement</h4>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single_member_counter">
+                        <span class="counter">160</span>
+                        <h4> Nombre de travailleurs sur la plate-forme</h4>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single_member_counter">
+                        <span class="counter">1005</span>
+                        <h4>Nombre d'épreuves disponible</h4>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single_member_counter">
+                        <span class="counter">720</span>
+                        <h4>Nombre d'élève inscrit</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- member_counter counter end -->
+
+
+
+    <!--::review_part start::-->
+    <section class="testimonial_part" style="position:relative;top:100px">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-xl-5">
+                    <div class="section_tittle text-center">
+                        <p>Avis de nos utilisateurs</p>
+                        <h2>Que pensez vous de Test request ?</h2>
+                    </div>
+                </div>
+            </div>
+            <!--Zone pour les témoignages-->
+            <div class="row">
+                <div class="col-md-12">
+                    <div id="testimonial-slider" class="owl-carousel">
+                        <div class="testimonial">
+                            <div class="pic">
+                                <img src="img/test_request_icone/etudiant_1.jpg"> 
+                                <h3 class="title">Romain</h3>
+                            </div>
+                            <div class="testimonial-profile">
+                                
+                                <span class="post">Etudiant à la FSA</span>
+                            </div>
+                            <p class="description">
+                           Coucou la famille ,  Test request m'a sauvé la vie .Je dispose maintenant grâce à cette plate-forme de toutes les ressources dont j'ai besoins dans mon apprentissage.Je recommande vraiment.Merci
+                             </p>
+                        </div>
+                        <div class="testimonial">
+                            <div class="pic">
+                                <img src="img/test_request_icone/etudiant_1.jpg">
+                                 <h3 class="title">Florence</h3>
+                            </div>
+                            <div class="testimonial-profile">
+                               
+                                <span class="">Elève en Terminale</span>
+                            </div>
+                            <p class="description">
+                            Je suis  élève en classe de Terminale et utilisateur de la plateforme TestRequest grâce à laquelle j'ai accès à de nombreuses épreuves .Je remercie et encourage les initiateurs de la-dite plateforme.Merci
+                            </p>
+                        </div>
+                        <div class="testimonial">
+                            <div class="pic">
+                                <img src="img/test_request_icone/etudiant_1.jpg">
+                                <h3 class="title">Bénédicte</h3>
+                            </div>
+                            <div class="testimonial-profile">
+                                
+                                <span class="post">Etudiante en Administration des Finances</span>
+                            </div>
+                            <p class="description">
+                            Salut , j'ai découvert tout récemment Test request et dès lors je l'ai adopté. Elle me facilite vraiment la vie.De ma chambre j'ai accès à une banque imprèssionnante de ressources .Je ne peux que remercié les initiateurs.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!--////           Fin du témoignage       //////-->
+            </div>
+    </section>
+
+
+    <!-- footer part start-->
+    <!-- footer part start-->
+<footer class="footer-area" style='position:relative;top:50px;' >
+    <div class="container" >
+        <div class="row justify-content-between">
+            <div class="col-sm-6 col-md-4 col-xl-3">
+                <div class="single-footer-widget footer_1">
+                    <a class="navbar-brand" href="index.html"> <img src="img/favicon.png" alt="logo" style='position:relative;bottom:15px;'><span style='color:black;font-size:19px;font-weight:bold;position:relative;bottom:30px;'>TestRequeste </span> </a>
+                    <p>Petit text à mettre ou une note ,je ne sais pas encore </p>
+
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-xl-4">
+                <div class="single-footer-widget footer_2">
+                    <h4>Newsletter</h4>
+                    <p>S'inscrire à notre Newsletter
+                    </p>
+                    <form action="#">
+                        <div class="form-group">
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder='Enter email address'
+                                    onfocus="this.placeholder = ''"
+                                    onblur="this.placeholder = 'Enter email address'">
+                                <div class="input-group-append">
+                                    <button class="btn btn_1" type="button"><i class="ti-angle-right"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                    <div class="social_icon">
+                        <a href="#"> <i class="ti-facebook"></i> </a>
+                        <a href="#"> <i class="ti-twitter-alt"></i> </a>
+                        <a href="#"> <i class="ti-instagram"></i> </a>
+                        <a href="#"> <i class="ti-skype"></i> </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 col-md-4">
+                <div class="single-footer-widget footer_2">
+                    <h4>Nous contactez</h4>
+                    <div class="contact_info">
+                        <p><span> Addresse :</span> A remplir </p>
+                        <p><span> Numéro :</span> +229  --------</p>
+                        <p><span> Email : </span>a_remplir.com </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="copyright_part_text text-center">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <p class="footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy; 2019 All rights reserved </p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+    <!-- footer part end-->
+
+    <!-- jquery plugins here-->
+    <!-- jquery -->
+    <script src="js/jquery-1.12.1.min.js"></script>
+    <!-- popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- easing js -->
+    <script src="js/jquery.magnific-popup.js"></script>
+    <!-- swiper js -->
+    <script src="js/swiper.min.js"></script>
+    <!-- swiper js -->
+    <script src="js/masonry.pkgd.js"></script>
+    <!-- particles js -->
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <!-- swiper js -->
+    <script src="js/slick.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <!-- custom js -->
+    <script src="js/custom.js"></script>
+
+    <script src="js/testimoy_slide.js"></script>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"></script>
+
+</body>
+
+</html>
